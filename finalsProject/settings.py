182dd01c9d8266e370c388bcbd13c398f292556e
JuniSama <ong2221692@mkt.ceu.edu.ph>
2024-05-16 23:33:57 +0800

    Meh

--- finalsProject/settings.py
+++ finalsProject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'finalsProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/ 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'finalsProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'finalsdb',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
